516e9372d714f55555d4ffbfc05dcf9f9e0ebf9d
Joe Graham <joegraham901+github@gmail.com>
2019-12-25 14:15:19 -0500

    Render upload

--- scoring_engine/checks/openvpn.py
+++ scoring_engine/checks/openvpn.py
@@ -2,7 +2,7 @@ from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
 
 
 class OpenVPNCheck(BasicCheck):
-    required_properties = ['ca']
+    required_properties = ['filename']
     CMD = CHECKS_BIN_PATH + '/openvpn_check {0} {1} {2} {3} {4}'
 
     def command_format(self, properties):
@@ -12,5 +12,5 @@ class OpenVPNCheck(BasicCheck):
             self.port,
             account.username,
             account.password,
-            properties['ca']
+            properties['filename']
         )
